5d7213af756e63c5a6831bd4b07bde689b72e6a4
jacob <jacobcunningham96@yahoo.com>
2020-06-18 11:41:30 -0600

    extra clean up

--- src/App.js
+++ src/App.js
@@ -1,9 +1,12 @@
 import React from "react";
 
+import { PostList } from "./components/PostList";
+
 const App = () => {
   return (
     <div className="ui container">
       <p>Hello World!</p>
+      <PostList />
     </div>
   );
 };
